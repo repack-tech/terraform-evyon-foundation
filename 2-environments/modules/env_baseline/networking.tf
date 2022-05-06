44a53ef2b1afefa204bb6c128238ddeb87b12937
Daniel Bunte [Evyon AS] <daniel.bunte.84+github@gmail.com>
2022-05-06 11:15:12 +0200

    Version updates + folder setting

--- 2-environments/modules/env_baseline/networking.tf
+++ 2-environments/modules/env_baseline/networking.tf
@@ -53,7 +53,7 @@ module "base_shared_vpc_host_project" {
 
 module "restricted_shared_vpc_host_project" {
   source                      = "terraform-google-modules/project-factory/google"
-  version                     = "~> 10.1"
+  version                     = "~> 13.0"
   random_project_id           = "true"
   impersonate_service_account = var.terraform_service_account
   name                        = format("%s-%s-shared-restricted", var.project_prefix, var.environment_code)
