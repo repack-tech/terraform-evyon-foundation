63fc9fe68e1325405a156371f52471cdcc02859a
Daniel Bunte [Evyon AS] <daniel.bunte.84+github@gmail.com>
2022-05-06 11:18:19 +0200

    removes "impersonate_service_account" setting, as it's non-existent anymore.

--- 2-environments/modules/env_baseline/networking.tf
+++ 2-environments/modules/env_baseline/networking.tf
@@ -22,7 +22,6 @@ module "base_shared_vpc_host_project" {
   source                      = "terraform-google-modules/project-factory/google"
   version                     = "~> 13.0"
   random_project_id           = "true"
-  impersonate_service_account = var.terraform_service_account
   name                        = format("%s-%s-shared-base", var.project_prefix, var.environment_code)
   org_id                      = var.org_id
   billing_account             = var.billing_account
@@ -55,7 +54,6 @@ module "restricted_shared_vpc_host_project" {
   source                      = "terraform-google-modules/project-factory/google"
   version                     = "~> 13.0"
   random_project_id           = "true"
-  impersonate_service_account = var.terraform_service_account
   name                        = format("%s-%s-shared-restricted", var.project_prefix, var.environment_code)
   org_id                      = var.org_id
   billing_account             = var.billing_account
